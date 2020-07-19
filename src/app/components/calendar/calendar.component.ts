dd20d0b4049dbef34e3032eed0e8f649740a4eaf
nazarskr <nazarskrypnyk.programmer@gmail.com>
2020-07-19 11:40:04 +0300

    Finished calendar rendering, fix modal init, create mixin for divider and add Prettier for project.

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MaterialInstance, MaterialService} from "../../shared/classes/material.service";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendar',
@@ -9,27 +10,60 @@ import {MaterialInstance, MaterialService} from "../../shared/classes/material.s
 export class CalendarComponent implements OnInit, AfterViewInit {
   @ViewChild('dayModal', {static: false}) dayModalRef: ElementRef;
   dayModal: MaterialInstance;
-
-  weeks = [1, 2, 3, 4];
-  days = [1,2,3,4,5,6,7];
-  modalValue: number;
+  currentMonth: any;
+  monthStart: any;
+  monthDays = [];
+  ukrainianWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота', 'Неділя']
 
   constructor() { }
 
   ngOnInit() {
+    this.currentMonth = moment();
+    this.setDaysNumbers(this.currentMonth);
   }
 
   ngAfterViewInit() {
-    MaterialService.initModal(this.dayModalRef);
+    this.dayModal = MaterialService.initModal(this.dayModalRef);
   }
 
   openModal (value) {
-    this.modalValue = value;
     this.dayModal.open();
+    console.log(value)
   }
 
   closeModal() {
     this.dayModal.close();
+  }
+
+  plusMonth() {
+    this.currentMonth.add(1, 'M');
+    this.setDaysNumbers(this.currentMonth);
+  }
+
+  minusMonth() {
+    this.currentMonth.subtract(1, 'M');
+    this.setDaysNumbers(this.currentMonth);
+  }
+
+  setDaysNumbers(month) {
+    const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    this.monthDays = [];
+    this.monthStart = month.startOf('month');
+    for (let i = 0; i < month.daysInMonth(); i++) {
+      this.monthDays.push(this.monthStart.clone().add(i,'days'));
+    }
+
+    const firstDayIndex = dayNames.indexOf(this.monthStart.format('dddd'));
+    const lastDay = this.monthDays[this.monthDays.length - 1];
+    const lastDayIndex = dayNames.indexOf(lastDay.format('dddd'));
+
+    for (let i = 0; i < firstDayIndex; i++) {
+      this.monthDays.unshift(this.monthStart.clone().subtract(i + 1,'days'));
+    }
+
+    for (let i = 1; i < 7 - lastDayIndex; i++) {
+      this.monthDays.push(lastDay.clone().add(i,'days'));
+    }
   }
 
 }
